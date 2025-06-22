d1e73165c184bc6031f6077ba1874f48d2b40a99
Main Projects(KAmarJahan) <95184009+futrengine@users.noreply.github.com>
2025-06-22 12:41:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.InlineKeyboardMarkup()
-    web_app = types.WebAppInfo("https://yourusername.github.io/spin-rewards-miniapp/")
+    web_app = types.WebAppInfo("https://futrengine.github.io/spinwheel")
     markup.add(types.InlineKeyboardButton("🎰 Open Spin", web_app=web_app))
     bot.send_message(message.chat.id, "Tap below to spin the wheel!", reply_markup=markup)
 
