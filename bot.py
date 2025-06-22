5333cd5c4d9b5b3cb277183fb3577d1369d30d0a
Main Projects(KAmarJahan) <95184009+futrengine@users.noreply.github.com>
2025-06-22 12:20:37 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,15 @@
+import telebot
+import os
+from telebot import types
+
+TOKEN = os.environ.get("BOT_TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.InlineKeyboardMarkup()
+    web_app = types.WebAppInfo("https://yourusername.github.io/spin-rewards-miniapp/")
+    markup.add(types.InlineKeyboardButton("🎰 Open Spin", web_app=web_app))
+    bot.send_message(message.chat.id, "Tap below to spin the wheel!", reply_markup=markup)
+
+bot.infinity_polling()
